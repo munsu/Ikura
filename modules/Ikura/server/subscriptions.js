464c2f387d5364cb26797588bc3fa2a4f1655dc6
Robin Miguel Anupol <ranupol@gmail.com>
2016-01-12 12:25:05 +0800

    Prevent public from accessing any data.

--- modules/Ikura/server/subscriptions.js
+++ modules/Ikura/server/subscriptions.js
@@ -2,9 +2,13 @@ import Loans from 'Ikura/collections/Loans';
 
 // This code only runs on the server
 Meteor.publish('loans', function () {
-  return Loans.find();
+  if (this.userId) {
+    return Loans.find();
+  }
 });
 
 Meteor.publish('userData', function () {
+  if (this.userId) {
     return Meteor.users.find();
+  }
 })
